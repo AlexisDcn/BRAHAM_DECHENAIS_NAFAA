2b88e6e0123f83dd6c3c16fb00c06f501135e0de
Alexis Dechenais <alexis.dechenais@etud.univ-jfc.fr>
2024-10-23 14:38:51 +0200

    premier commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,10 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
